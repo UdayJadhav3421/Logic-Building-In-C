3fd72320e4187ae4c399714063a304ec7857ed72
UdayJadhav3421 <udaysjadhav3@gmail.com>
2021-12-22 19:06:04 +0530

    Logic Buildind In C

---
+++ Assignment_01/02 Program To Print  Jay Ganesh  5 Times.c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void Display()
+{
+    int i = 0;
+    for(i = 0; i < 5; i++)
+    {
+        printf("\n\t Jay Ganesh!!!\n\n");
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
