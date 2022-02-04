d912a9c62bc632c5cef36be6c088d7b08158bffa
UdayJadhav3421 <udaysjadhav3@gmail.com>
2022-02-04 22:36:18 +0530

    Logic Building In C

---
+++ Assignment_13/05. Accept Number Form User And Return Difference Bteween Summation Of Even Digits And Summaion Of Odd Digits.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int CountDiff(int No)
+{
+
+
+}
+int main()
+{
+    int Value = 0;
+
+    printf("\n Enter a Number => ");
+    scanf("%d",&Value);
+
+    printf("\n %d",CountDiff(Value));
+
+    getch();
+    return 0;
+}
