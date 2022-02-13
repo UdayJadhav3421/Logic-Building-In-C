bf041b162feec4f17b3918d8b6361e94a0fb54d4
UdayJadhav3421 <udaysjadhav3@gmail.com>
2022-02-13 23:28:47 +0530

    Logic Building In C

---
+++ Assignment_17/05. Accept Number Of Rows And Number Of Columns From User And Display Below Pattern.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int Row, int Col)
+{
+    int i = 0, j = 0;
+
+    for(i = 0; i < Row; i++)
+    {
+        for(j = 1; j <= Col; j++)
+        {
+            printf(" %3d ",j+i);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int Value1,Value2;
+
+    printf("\n Enter a Row Number => ");
+    scanf("%d",&Value1);
+
+    printf("\n Enter a Column Number => ");
+    scanf("%d",&Value2);
+
+    Pattern(Value1,Value2);
+
+    getch();
+    return 0;
+}
