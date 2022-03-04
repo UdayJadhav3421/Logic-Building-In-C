716d66786def49f7ae7ce70226cb26c56fdecddb
UdayJadhav3421 <udaysjadhav3@gmail.com>
2022-03-04 21:57:10 +0530

    Logic Building In C

---
+++ Assignment_23/02. Accept N Number FRom  User And One Another Number As NO, Return index Of First Occurrence Of That NO.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+
+int FirstOcc(int Arr[],int Length, int No)
+{
+    int i = 0;
+    for(i = 0; i < Length; i++)
+    {
+        if(Arr[i] == No)
+        {
+            return i;
+        }
+    }
+   return -1;
+}
+int main()
+{
+    int Size = 0,Cnt = 0, bRet = 0,Value = 0;
+    int *p = NULL;
+
+    printf("\n Enter Number Of Elements => ");
+    scanf("%d",&Size);
+
+    printf("\n Enter The Number => ");
+    scanf("%d",&Value);
+
+    p = (int *)malloc(Size * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable To Allocate Memory");
+        return -1;
+    }
+
+    printf("\n\t Enter %d Elements => ",Size);
+
+    for(Cnt = 0; Cnt < Size; Cnt++)
+    {
+        printf("\n\t Enter Element: %d => ",Cnt + 1);
+        scanf("%d",&p[Cnt]);
+    }
+
+     printf("\n\t The Number Is %d.",FirstOcc(p, Size,Value));
+
+     free(p);
+
+     getch();
+     return 0;
+}
